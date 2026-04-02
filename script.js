8558ead4cd06c80798af1f2570126efb40ae8572
Dhayalan <dhayalanspace@gmail.com>
2026-04-02 14:39:03 +0530

    Add image upload functionality for meme creation

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function generateMeme() {
     document.getElementById("top").innerText = topText;
     document.getElementById("bottom").innerText = bottomText;
 }
+document.getElementById("imageInput").addEventListener("change", function(event) {
+    const file = event.target.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+        
+        reader.onload = function(e) {
+            document.getElementById("memeImage").src = e.target.result;
+        };
+        
+        reader.readAsDataURL(file);
+    }
+});
