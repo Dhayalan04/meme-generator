7f7eabc66a96b8948cae5820f19c9537a96db1db
Dhayalan <dhayalanspace@gmail.com>
2026-04-05 00:31:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,8 @@ const templateSelect = document.getElementById("template-select");
 const generateBtn = document.getElementById("generate-btn");
 const downloadBtn = document.getElementById("download-btn");
 const randomBtn = document.getElementById("random-btn");
-const shareBtn = document.getElementById("share-btn");
 const toggleTheme = document.getElementById("toggleTheme");
 const templatePreview = document.getElementById("template-preview");
-const historyContainer = document.getElementById("history-container");
 
 let currentImage = new Image();
 let topText = "TOP TEXT";
@@ -61,24 +59,14 @@ imageUpload.addEventListener("change", (e) => {
 });
 
 // Text inputs
-inputTop.addEventListener("input", () => {
-  topText = inputTop.value;
-  topTextDiv.textContent = topText;
-});
-inputBottom.addEventListener("input", () => {
-  bottomText = inputBottom.value;
-  bottomTextDiv.textContent = bottomText;
-});
+inputTop.addEventListener("input", () => { topText = inputTop.value; topTextDiv.textContent = topText; });
+inputBottom.addEventListener("input", () => { bottomText = inputBottom.value; bottomTextDiv.textContent = bottomText; });
 fontSizeInput.addEventListener("input", drawMeme);
 fontColorInput.addEventListener("input", drawMeme);
 fontStyleInput.addEventListener("input", drawMeme);
 
 // Buttons
-generateBtn.addEventListener("click", () => {
-  drawMeme();
-  saveMemeToHistory();
-});
-
+generateBtn.addEventListener("click", drawMeme);
 downloadBtn.addEventListener("click", () => {
   html2canvas(document.getElementById("meme-container")).then(canvas => {
     const link = document.createElement("a");
@@ -87,121 +75,53 @@ downloadBtn.addEventListener("click", () => {
     link.click();
   });
 });
-
 randomBtn.addEventListener("click", () => {
   const randomMeme = memeTemplates[Math.floor(Math.random() * memeTemplates.length)];
   templateSelect.value = randomMeme.url;
   loadImage(randomMeme.url);
 });
 
-// Share meme
-shareBtn.addEventListener("click", () => {
-  html2canvas(document.getElementById("meme-container")).then(canvas => {
-    const dataURL = canvas.toDataURL();
-    const encodedURL = encodeURIComponent(dataURL);
-    const twitterURL = `https://twitter.com/intent/tweet?text=Check out my meme!&url=${encodedURL}`;
-    window.open(twitterURL, "_blank");
-  });
-});
-
 // Drag & drop
 function dragElement(el) {
-  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  let pos1=0,pos2=0,pos3=0,pos4=0;
   el.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    el.style.top = (el.offsetTop - pos2) + "px";
-    el.style.left = (el.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+  function dragMouseDown(e) { e.preventDefault(); pos3=e.clientX; pos4=e.clientY;
+    document.onmouseup=closeDragElement; document.onmousemove=elementDrag; }
+  function elementDrag(e) { e.preventDefault(); pos1=pos3-e.clientX; pos2=pos4-e.clientY;
+    pos3=e.clientX; pos4=e.clientY;
+    el.style.top=(el.offsetTop-pos2)+"px"; el.style.left=(el.offsetLeft-pos1)+"px"; }
+  function closeDragElement() { document.onmouseup=null; document.onmousemove=null; }
 }
-
 dragElement(topTextDiv);
 dragElement(bottomTextDiv);
 
 // Load image
-function loadImage(src) {
-  currentImage.src = src;
-  currentImage.crossOrigin = "anonymous";
+function loadImage(src){
+  currentImage.src = src; currentImage.crossOrigin="anonymous";
   currentImage.onload = () => {
-    canvas.width = currentImage.width;
-    canvas.height = currentImage.height;
-    topTextDiv.style.top = "10px";
-    bottomTextDiv.style.top = canvas.height - 60 + "px";
-    topTextDiv.style.left = "0px";
-    bottomTextDiv.style.left = "0px";
+    canvas.width = currentImage.width; canvas.height = currentImage.height;
+    topTextDiv.style.top = "10px"; bottomTextDiv.style.top = canvas.height - 60 + "px";
+    topTextDiv.style.left = "0px"; bottomTextDiv.style.left = "0px";
     drawMeme();
   };
 }
 
 // Draw meme
-function drawMeme() {
-  if (!currentImage.src) return;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(currentImage, 0, 0, canvas.width, canvas.height);
+function drawMeme(){
+  if(!currentImage.src) return;
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.drawImage(currentImage,0,0,canvas.width,canvas.height);
 
-  const fontSize = parseInt(fontSizeInput.value, 10);
-  const fontColor = fontColorInput.value;
-  const fontStyle = fontStyleInput.value;
+  const fontSize=parseInt(fontSizeInput.value,10);
+  const fontColor=fontColorInput.value;
+  const fontStyle=fontStyleInput.value;
 
-  ctx.font = `${fontSize}px ${fontStyle}`;
-  ctx.fillStyle = fontColor;
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 2;
-  ctx.textAlign = "center";
+  ctx.font=`${fontSize}px ${fontStyle}`;
+  ctx.fillStyle=fontColor; ctx.strokeStyle="black"; ctx.lineWidth=2; ctx.textAlign="center";
 
-  ctx.fillText(topTextDiv.textContent, canvas.width / 2, parseInt(topTextDiv.style.top || 50) + fontSize);
-  ctx.strokeText(topTextDiv.textContent, canvas.width / 2, parseInt(topTextDiv.style.top || 50) + fontSize);
+  ctx.fillText(topTextDiv.textContent,canvas.width/2,parseInt(topTextDiv.style.top||50)+fontSize);
+  ctx.strokeText(topTextDiv.textContent,canvas.width/2,parseInt(topTextDiv.style.top||50)+fontSize);
 
-  ctx.fillText(bottomTextDiv.textContent, canvas.width / 2, parseInt(bottomTextDiv.style.top || (canvas.height - 50)) + fontSize);
-  ctx.strokeText(bottomTextDiv.textContent, canvas.width / 2, parseInt(bottomTextDiv.style.top || (canvas.height - 50)) + fontSize);
-}
-
-// Meme history
-let memeHistory = JSON.parse(localStorage.getItem("memeHistory") || "[]");
-renderHistory();
-
-function saveMemeToHistory() {
-  html2canvas(document.getElementById("meme-container")).then(canvas => {
-    const dataURL = canvas.toDataURL();
-    memeHistory.unshift(dataURL);
-    localStorage.setItem("memeHistory", JSON.stringify(memeHistory));
-    renderHistory();
-  });
-}
-
-function renderHistory() {
-  historyContainer.innerHTML = "";
-  memeHistory.forEach((src) => {
-    const img = document.createElement("img");
-    img.src = src;
-    img.classList.add("history-thumbnail");
-    img.title = "Click to download";
-    img.addEventListener("click", () => downloadHistoryMeme(src));
-    historyContainer.appendChild(img);
-  });
-}
-
-function downloadHistoryMeme(src) {
-  const link = document.createElement("a");
-  link.href = src;
-  link.download = "meme.png";
-  link.click();
+  ctx.fillText(bottomTextDiv.textContent,canvas.width/2,parseInt(bottomTextDiv.style.top||(canvas.height-50))+fontSize);
+  ctx.strokeText(bottomTextDiv.textContent,canvas.width/2,parseInt(bottomTextDiv.style.top||(canvas.height-50))+fontSize);
 }
