476d4d83db0a74edbbf98d4a79e6860a232faba0
Dhayalan <dhayalanspace@gmail.com>
2026-04-04 09:56:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ async function generateAIImage() {
   try {
     const prompt = document.getElementById("aiPrompt").value;
 
-    const res = await fetch("https://meme-backend-n1g8.onrender.com/generate-image", {
+    const res = await fetch("https://meme-generator-fp80.onrender.com/generate-image", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
